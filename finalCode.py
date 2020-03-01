e6a0d3de9f43c8e6583d5c661bde98e0561db112
Rmin-Sadigh <armin_sadigh@yahoo.com>
2020-03-01 19:16:19 +0330

    Initial Stage

---
+++ finalCode.py
@@ -0,0 +1,179 @@
+############################################
+####  Parking AI by Armin Ghayursadigh  ####
+############################################
+
+
+# Declaring objects and variables
+
+
+import numpy as np
+import copy
+
+#############################################################
+# Edit this list according to your problem
+# first array is [rows,colums,number of cars]
+# the rest of the arrays are car positions
+# make sure the first car is the car you want to get out
+# exiting the parking is defined as reaching the right edge
+# of the parking so plan your cars accordingly
+#############################################################
+
+initList = [
+[6,6,8],
+[3,1,'h',2],
+[1,2,'v',2],
+[1,4,'h',3],
+[2,5,'h',2],
+[3,4,'v',2],
+[5,3,'h',2],
+[5,5,'v',2],
+[5,6,'v',2]
+]
+
+minMovesReq = 500
+idCounter = 0
+
+rows = initList[0][0]
+cols = initList[0][1]
+carNum = initList[0][2]
+
+initSchema = np.zeros((rows,cols),dtype=int)
+
+initList.pop(0)
+
+states = list()
+successors = list()
+
+class state:
+    def __init__(self, lst, step):
+        self.minMoves = step
+        global minMovesReq
+        global idCounter
+
+        idCounter = idCounter + 1
+        self.ID = idCounter
+        
+        if self.minMoves >= minMovesReq:
+            return
+        self.lst = lst
+        self.cars = list()
+        for i in range(1,len(lst)+1):
+            self.cars.append(car(i,lst[i-1]))
+        self.schema = np.zeros((rows,cols),dtype=int)
+        for carObj in self.cars:
+            if carObj.orientation[2] == 'v':
+                col = carObj.orientation[1] - 1
+                startRow = carObj.orientation[0] - 1
+                endRow = startRow + carObj.orientation[3] -1
+                for j in range(startRow,endRow+1):
+                    self.schema[j][col]=carObj.ID
+            else:
+                row = carObj.orientation[0] - 1
+                startCol = carObj.orientation[1] - 1
+                endCol = startCol + carObj.orientation[3] -1
+                for j in range(startCol,endCol+1):
+                    self.schema[row][j]=carObj.ID
+        self.possibleMoves = dict()
+        for carObj in self.cars:
+            self.possibleMoves[carObj.ID] = carObj.calcMoves(self.schema)
+            if self.possibleMoves[carObj.ID] == []:
+                del self.possibleMoves[carObj.ID]
+
+    def moveCar(self, carID, moveStep):
+        return self.cars[carID-1].move(copy.deepcopy(self.lst),moveStep)
+
+    def checkUnique(self,checkList):
+        for stateObj in states:
+            if np.array_equal(stateObj.lst,checkList):
+                return False
+            else:
+                continue
+        return True
+
+    def relief(self):
+        for stateObj in states:
+            if np.array_equal(stateObj.schema,self.schema):
+                if stateObj.minMoves > self.minMoves:
+                    stateObj.minMoves = copy.deepcopy(self.minMoves)
+                    return
+
+    def checkSucc(self):
+        if self.schema[initList[0][0]-1][cols-1] == 1:
+            return True
+        else:
+            return False
+
+class car:
+    def __init__(self, ID, orientation):
+        self.ID = ID
+        self.orientation = orientation
+    def calcMoves(self,schema):
+        domain = []
+        if self.orientation[2] == 'v':
+            col = self.orientation[1] - 1
+            startRow = self.orientation[0] - 1
+            endRow = startRow + self.orientation[3] -1
+
+            for i in range(startRow-1,-1,-1):
+                if schema[i][col] == 0:
+                    domain.append(i-startRow)
+                else:
+                    break
+
+            for i in range(endRow+1,rows,1):
+                if schema[i][col] == 0:
+                    domain.append(i-endRow)
+                else:
+                    break
+        else:
+            row = self.orientation[0] - 1
+            startCol = self.orientation[1] - 1
+            endCol = startCol + self.orientation[3] -1
+
+            for i in range(startCol-1,-1,-1):
+                if schema[row][i] == 0:
+                    domain.append(i-startCol)
+                else:
+                    break
+
+            for i in range(endCol+1,cols,1):
+                if schema[row][i] == 0:
+                    domain.append(i-endCol)
+                else:
+                    break
+
+        return sorted(domain, reverse = True)
+
+    def move(self,lst,moveStep):
+        if lst[self.ID-1][2] == 'v':
+            lst[self.ID-1][0] = lst[self.ID-1][0] + moveStep
+            return lst
+        else:
+            lst[self.ID-1][1] = lst[self.ID-1][1] + moveStep
+            return lst
+
+# Initializing the calculations
+
+states.append(state(initList,0))
+
+# Creating new states in a loop
+
+print("Please wait while calculations are being done")
+
+for stateObj in states:
+    for key, value in stateObj.possibleMoves.items():
+        for steps in value:
+            if(stateObj.minMoves >= minMovesReq):
+                break
+            if(stateObj.checkUnique(stateObj.moveCar(key, steps))):
+                if(key == 1 and steps > 0):
+                    states.insert(states.index(stateObj)+1, state(stateObj.moveCar(key, steps), copy.deepcopy(stateObj.minMoves) + 1))
+                else:
+                    states.append(state(stateObj.moveCar(key, steps), copy.deepcopy(stateObj.minMoves) + 1))
+                if(stateObj.checkSucc()):
+                    successors.append(stateObj)
+                    minMovesReq = copy.deepcopy(min(succ.minMoves for succ in successors))
+                    print("min moves required to solve this puzzle is= {}".format(stateObj.minMoves))
+                    exit()
+            else:
+                state(stateObj.moveCar(key, steps), copy.deepcopy(stateObj.minMoves) + 1).relief()
